7aa8d6c228b09bedf9cec52b66193aae1df40bf5
sandra stark <stark.sandy@gmail.com>
2015-02-18 09:47:21 -0800

    able to save drawing to database

---
+++ app/assets/javascripts/drawings.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $('#save').onclick(function(){
+      var canvas = document.getElementById("canvas");
+      var dataURL = canvas.toDataURL();
+      console.log(dataURL);
+  });
+});
--- app/controllers/drawings_controller.rb
+++ app/controllers/drawings_controller.rb
@@ -1,5 +1,7 @@
 class DrawingsController < ApplicationController
+ 
   def index
+    @drawings = Drawing.all.order(created_at: :desc)
   end
 
   def show
@@ -9,5 +11,12 @@ class DrawingsController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    # new_drawing = params.require(:drawing).permit(:drawing)
+    image_string = params[:drawing]
+    new_drawing= Drawing.create({drawing: image_string})
+    render :json => new_drawing
   end
 end
